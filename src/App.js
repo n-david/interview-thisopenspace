a7d390a4d1f6a427f0a7299591fc54f3868b49b4
n-david <ngdavid.93@gmail.com>
2018-03-25 18:50:58 -0700

    in person interview changes, fetch all listings and sort

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 //***** COMPONENTS *****
 import Loading from './Components/Loading';
-// import SortBy from './SortBy/SortBy';
+import SortBy from './SortBy/SortBy';
 import Listing from './Listing/Listing';
 import Pages from './Pages/Pages';
 
 class App extends Component {
   state = {
-    listings: null,
+    listings: [],
     pageSize: null,
     total: null,
     loading: true,
@@ -48,9 +48,28 @@ class App extends Component {
   };
 
   //***** HELPERS *****
-  fetchListings = async () => {
-    this.setState({ loading: true });
+  // fetchListings = async () => {
+  //   this.setState({ loading: true });
+  //
+  //   try {
+  //     const response = await fetch(
+  //       `${this.prodENV ? 'api' : ''}/lhl-test?page=${this.state.currentPage}`
+  //     );
+  //     const listings = await response.json();
+  //     console.log(listings);
+  //
+  //     this.setState({
+  //       listings: listings.data,
+  //       pageSize: listings.page_size,
+  //       total: listings.total,
+  //       loading: false,
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
+  fetchListings = async () => {
     try {
       const response = await fetch(
         `${this.prodENV ? 'api' : ''}/lhl-test?page=${this.state.currentPage}`
@@ -58,34 +77,68 @@ class App extends Component {
       const listings = await response.json();
       console.log(listings);
 
-      this.setState({
-        listings: listings.data,
-        pageSize: listings.page_size,
-        total: listings.total,
-        loading: false,
-      });
+      this.setState(
+        prevState => ({ listings: [...prevState.listings, ...listings.data] }),
+        () => {
+          this.fetchMore(listings.total);
+        }
+      );
     } catch (err) {
       console.log(err);
     }
   };
 
+  fetchMore = total => {
+    if (this.state.listings.length !== total) {
+      this.setState(
+        prevState => ({ currentPage: prevState.currentPage + 1 }),
+        () => {
+          this.fetchListings();
+        }
+      );
+    } else {
+      this.setState({ loading: false });
+      console.log('no more');
+    }
+  };
+
+  sortBySqft = () => {
+    const listings = this.state.listings.sort((a, b) => {
+      return b.square_footage - a.square_footage;
+    });
+
+    this.setState({ listings });
+    console.log(listings);
+  };
+
   // fetchAllListings = async () => {
   //   try {
-  //     const pageOne = await fetch('http://localhost:3000/lhl-test?page=1');
-  //     const pageOneJSON = await pageOne.json();
-  //     const pageTwo = await fetch('http://localhost:3000/lhl-test?page=2');
-  //     const pageTwoJSON = await pageTwo.json();
-  //     const pageThree = await fetch('http://localhost:3000/lhl-test?page=3');
-  //     const pageThreeJSON = await pageThree.json();
-  //     const pageFour = await fetch('http://localhost:3000/lhl-test?page=4');
-  //     const pageFourJSON = await pageFour.json();
+  //   	// for ()
+  //     const listings = await fetch('http://localhost:3000/lhl-test?page=1');
+  //     const listingsJSON = await listings.json();
+  //     console.log(listingsJSON);
   //
-  //     console.log([
-  //       ...pageOneJSON.data,
-  //       ...pageTwoJSON.data,
-  //       ...pageThreeJSON.data,
-  //       ...pageFourJSON.data,
-  //     ]);
+  //    // this.setState({prevState => (
+  //     	// listings: [],
+  //    //   total: listingsJSON.total,
+  //    //   currentPage: listingsJSON
+  //    // )});
+  //
+  //     // if (listingsJSON.page_size * this.state.currentPage >= listingsJSON.total) {}
+  //
+  //     // const pageTwo = await fetch('http://localhost:3000/lhl-test?page=2');
+  //     // const pageTwoJSON = await pageTwo.json();
+  //     // const pageThree = await fetch('http://localhost:3000/lhl-test?page=3');
+  //     // const pageThreeJSON = await pageThree.json();
+  //     // const pageFour = await fetch('http://localhost:3000/lhl-test?page=4');
+  //     // const pageFourJSON = await pageFour.json();
+  //
+  //     // console.log([
+  //     //   ...pageOneJSON.data,
+  //     //   ...pageTwoJSON.data,
+  //     //   ...pageThreeJSON.data,
+  //     //   ...pageFourJSON.data,
+  //     // ]);
   //   } catch (err) {
   //     console.log(err);
   //   }
@@ -96,32 +149,28 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header" onClick={this.sortBySqft}>
           <img src={thisopenspace} className="App-logo" alt="logo" />
           <h1 className="App-title">thisopenspace</h1>
         </header>
-        {/*<div className="sort-by-container">*/}
-        {/*Show me <SortBy /> spaces*/}
-        {/*</div>*/}
-        <div className="listings-container">
-          {loading ? (
-            <Loading />
-          ) : (
-            listings.map(listing => (
-              <Listing key={listing.id} listing={listing} />
-            ))
-          )}
+        <div className="sort-by-container">
+          Show me <SortBy /> spaces
         </div>
-        {!loading && (
-          <footer className="App-footer">
-            <Pages
-              pageSize={pageSize}
-              total={total}
-              currentPage={currentPage}
-              onClickChangePage={this.onClickChangePage}
-              onClickJumpToPage={this.onClickJumpToPage}
-            />
-          </footer>
+        <div className="listings-container">
+          {listings.map(listing => (
+            <Listing key={listing.id} listing={listing} />
+          ))}
+        </div>
+        {/*{!loading && (*/}
+        {/*<footer className="App-footer">*/}
+        {/*<Pages*/}
+        {/*pageSize={pageSize}*/}
+        {/*total={total}*/}
+        {/*currentPage={currentPage}*/}
+        {/*onClickChangePage={this.onClickChangePage}*/}
+        {/*onClickJumpToPage={this.onClickJumpToPage}*/}
+        {/*/>*/}
+        {/*</footer>*/}
         )}
       </div>
     );
